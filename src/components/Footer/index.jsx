d0c931b4b24707745e0cb46f2e16e3c982036e00
Gustavo <gustavosfischer@gmail.com>
2025-09-01 21:56:45 -0300

    feat/fetch from original rick and morty api

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,9 @@
+import './style.css'
+
+export default function Footer() {
+    return (
+        <footer>
+            <p>&copy; 2025 - Provavelmente agora já deve ter algum direito reservado - Gustavo S. Fischer</p>
+        </footer>
+    )
+}
