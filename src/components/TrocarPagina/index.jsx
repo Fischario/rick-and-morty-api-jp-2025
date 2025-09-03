666390f70f7ca1bf557c6a20768dc615824c4199
Gustavo <gustavosfischer@gmail.com>
2025-09-03 19:27:19 -0300

    feat/init page changing

---
+++ src/components/TrocarPagina/index.jsx
@@ -0,0 +1,10 @@
+import './style.css'
+
+export default function TrocarPagina() {
+    return (
+        <div className='trocar-pagina'>
+            <button id='pagina-anterior'>Página anterior</button>
+            <button id='proxima-pagina'>Próxima página</button>
+        </div>
+    )
+}
